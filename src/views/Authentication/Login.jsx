3f8ebb07a6d930ac8c82f642e1cb4ab2c72c7add
Miss-Siddhi-Pagire <siddhipagire825@gmail.com>
2025-02-15 23:14:25 +0530

    image added

--- src/views/Authentication/Login.jsx
+++ src/views/Authentication/Login.jsx
@@ -78,10 +78,12 @@ export default function Login() {
           </button>
         </div>
       </div>
+
       <div>
         <img src=''></img>
       </div>
-    </div>
+      
+          </div>
     </>
   );
 }
