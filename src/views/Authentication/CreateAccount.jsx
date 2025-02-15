e491a27b9231a5345e0fe0ee82699b613c566385
Miss-Siddhi-Pagire <siddhipagire825@gmail.com>
2025-02-15 23:45:23 +0530

    new train image added

--- src/views/Authentication/CreateAccount.jsx
+++ src/views/Authentication/CreateAccount.jsx
@@ -39,93 +39,100 @@ export default function CreateAccount() {
   };
   return (
     <div className="min-h-screen flex items-center justify-center  mt-33 mb-10">
-      <div className="bg-white p-8 rounded-xl shadow-lg w-96">
-        <h2 className="text-[32px] font-bold text-[#333333] text-center mb-4">CREATE ACCOUNT</h2>
-        <form onSubmit={handleSubmit}>
-          {error && <div className="text-red-500 text-center mb-4">{error}</div>}
-          <div className="mb-4">
-            <label className="block text-[#333333] text-sm font-medium">Username</label>
-            <input
-              type="text"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
-              placeholder="Enter username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-[#333333] text-sm font-medium">Full Name</label>
-            <input
-              type="text"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
-              placeholder="Enter full name"
-              value={fullname}
-              onChange={(e) => setFullname(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-[#333333] text-sm font-medium">Email</label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-[#333333] text-sm font-medium">Mobile Number</label>
-            <input
-              type="text"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
-              placeholder="Enter mobile number"
-              value={mobile}
-              onChange={(e) => setMobile(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-[#333333] text-sm font-medium">Password</label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
-              placeholder="Enter password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-[#333333] text-sm font-medium">Confirm Password</label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
-              placeholder="Confirm your password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
+      <div className="bg-white  rounded-xl shadow-lg w-250 flex">
+        <div>
+          <img src='src\assets\images\train2.avif' className='h-106 rounded-l-xl'></img>
+        </div>
+        <div className='ml-10 p-5'>
+          <h2 className="text-[32px] font-bold text-[#333333] text-center mb-4">CREATE ACCOUNT</h2>
+          <form onSubmit={handleSubmit}>
+            {error && <div className="text-red-500 text-center mb-4">{error}</div>}
+            <div className="mb-4">
+              <label className="block text-[#333333] text-sm font-medium">Username</label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 w-200 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
+                placeholder="Enter username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-[#333333] text-sm font-medium">Full Name</label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
+                placeholder="Enter full name"
+                value={fullname}
+                onChange={(e) => setFullname(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-[#333333] text-sm font-medium">Email</label>
+              <input
+                type="email"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-[#333333] text-sm font-medium">Mobile Number</label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
+                placeholder="Enter mobile number"
+                value={mobile}
+                onChange={(e) => setMobile(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-[#333333] text-sm font-medium">Password</label>
+              <input
+                type="password"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-[#333333] text-sm font-medium">Confirm Password</label>
+              <input
+                type="password"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-[#008000]"
+                placeholder="Confirm your password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <button
+                type="submit"
+                className="w-full bg-[#008000] text-white py-3 rounded-lg hover:bg-green-700"
+              >
+                REGISTER
+              </button>
+            </div>
+          </form>
+          <div className="flex justify-between mt-4">
             <button
-              type="submit"
-              className="w-full bg-[#008000] text-white py-3 rounded-lg hover:bg-green-700"
+              className="w-[45%] bg-[#004080] text-white py-2 rounded-lg hover:bg-blue-800"
+              onClick={() => navigate('/login')}
             >
-              REGISTER
+              BACK TO LOGIN
+            </button>
+            <button
+              className="w-[45%] bg-[#FFA500] text-white py-2 rounded-lg hover:bg-orange-100"
+              onClick={() => navigate('/forgot-account')}
+            >
+              FORGOT PASSWORD?
             </button>
           </div>
-        </form>
-        <div className="flex justify-between mt-4">
-          <button 
-            className="w-[45%] bg-[#004080] text-white py-2 rounded-lg hover:bg-blue-800"
-            onClick={() => navigate('/login')} 
-          >
-            BACK TO LOGIN
-          </button>
-          <button 
-            className="w-[45%] bg-[#FFA500] text-white py-2 rounded-lg hover:bg-orange-100"
-            onClick={() => navigate('/forgot-account')}
-          >
-            FORGOT PASSWORD?
-          </button>
+
         </div>
+
       </div>
     </div>
   );
