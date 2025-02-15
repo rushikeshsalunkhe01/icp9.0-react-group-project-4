627745398c1b6c89e11ec587929a1c33e3de41a3
Vaishnavi Bhawar <vaishnavibhawar1201@gmail.com>
2025-02-15 21:19:06 +0530

    img addded

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Correct import
 import Navbar from "./components/Navbar";
 import Home from "./views/Home";
+import PrnStatus from "./views/prntraker/prntraker"; // 
 import TrainDetails from "./views/TrainSearch/TrainDetails";
 import SupportView from "./views/support/SupportView";
 import FAQs from "./views/support/FAQs"; // Imported but not used
@@ -12,6 +13,8 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} /> {/* Home Page */}
+        <Route path="/pnr-status" element={<PrnStatus />} /> {/* PNR Status Page */}
+        
         <Route path="/train-details" element={<TrainDetails />} /> {/* Train Details Page */}
         <Route path="/support" element={<SupportView />} /> {/* Support Page */}
         <Route path="/faqs" element={<FAQs />} /> {/* FAQs Page */}
