23d450a61af585bd6be54097be336d9a1bada4a7
Vaishnavi Bhawar <vaishnavibhawar1201@gmail.com>
2025-02-15 21:30:40 +0530

    make changes

---
+++ src/views/prntraker/prntraker.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import trainGif from "./../../assets/images/prntrakerimg/train.webp";
+import Navbar from "../../components/Navbar";
+
