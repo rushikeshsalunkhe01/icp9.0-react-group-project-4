0491ffe8f61d97be94300427ff959eb3ebc450a1
Rushikesh salunkhe <rushisalunkhe111053@gmail.com>
2025-02-15 10:33:43 +0530

    update support

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { ChevronDown, Menu, X } from "lucide-react";
 import Logo from "../assets/images/logo.png";
--- src/views/support/FAQs.jsx
+++ src/views/support/FAQs.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const FAQs = () => {
   const [faqs, setFaqs] = useState([]);
