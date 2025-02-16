5f0ab4d61d4df040f2706357f16a590430b0a6b2
Rushikesh salunkhe <rushisalunkhe111053@gmail.com>
2025-02-16 09:59:57 +0530

    prerequisite change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import About from "./views/About";
-import Home from "./src/Home"; // Correct relative path
-
+import About from "./views/About.jsx";
+import Home from "./Home.jsx";
 import TrainExplorer from "./views/TrainSearch/TrainExplorer";
 import SupportView from "./views/support/SupportView";
 import Booking from "./views/Booking/Booking";
+import RegisterForm from "./views/auth/Register.jsx"; 
+import LoginForm from "./views/auth/Login.jsx"; 
+import ForgotAccDetails from "./views/auth/ForgateAccDetails.jsx";  {/* Forgot Account Details */}
+
 import TrainDetails from "./views/TrainSearch/TrainDetails";
 import PassengerForm from "./views/Booking/PassengerDetails";
 import FAQs from "./views/support/FAQs"; 
 import Payment from "./views/Booking/Payment";
-import Login from "./views/Authentication/Login";
-import CreateAccount from "./views/Authentication/CreateAccount";
-import ForgotAccount from "./views/Authentication/ForgotAccount";
 
 const App = () => {
   return (
@@ -22,19 +22,16 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} /> {/* Home Page */}
         <Route path="/train-explorer" element={<TrainExplorer />} /> 
-        <Route path="/support" element={<SupportView />} />{/* Support Page */}
+        <Route path="/support" element={<SupportView />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/ForgateAccDetails" element={<ForgotAccDetails />} /> {/* Route for Forgot Account */}
         <Route path="/booking" element={<Booking />} />
         <Route path="/about" element={<About />} />
         <Route path="/passenger-details" element={<PassengerForm />} />
-        <Route path="/faqs" element={<FAQs />} /> {/* FAQs Page */}
+        <Route path="/faqs" element={<FAQs />} />
         <Route path="/train-explorer/train-detail/:id" element={<TrainDetails />} />
         <Route path="/payment" element={<Payment />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<CreateAccount/>} />
-        <Route path="/forgot-account" element={<ForgotAccount/>} />
-        
-        
-
       </Routes>
       <Footer />
     </Router>
